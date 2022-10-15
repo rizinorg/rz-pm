e7232d2482fc7faf7ca8b1ccf35bb33c421fdd54
Riccardo Schirone <rschirone91@gmail.com>
2022-10-16 01:10:39 +0200

    Fix release CI and update app version to 0.1.3

--- main.go
+++ main.go
@@ -70,7 +70,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "rz-pm"
 	app.Usage = "rizin package manager"
-	app.Version = "0.1.1"
+	app.Version = "0.1.3"
 
 	app.Flags = []cli.Flag{
 		&cli.BoolFlag{
